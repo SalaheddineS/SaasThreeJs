a7b45bacc351e9c81c9aa59b9e3d3c10155a013b
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-07-19 11:53:48 +0100

    Structured services

--- src/Services/index.ts
+++ src/Services/index.ts
@@ -1,5 +1,18 @@
 import ImageService from './ImageService';
+import _3DModel from '../Types/_3D_Model';
+import ProjectService from './ProjectService';
+import SceneService from './ScenesService';
+import SongService from './SongService';
+import TextService from './TextService';
+import VideoService from './VideoService';
+import UserService from './UserService';
 
 export default {
-    ImageService
+    ImageService,
+    ProjectService,
+    SceneService,
+    SongService,
+    TextService,
+    VideoService,
+    UserService
 }
