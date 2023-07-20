875e426c989a6d888ee2fc7fbe39d74d73d12b30
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-07-20 15:36:17 +0100

    Finished 3DModel Controller

--- src/Controllers/_3DModelController/index.ts
+++ src/Controllers/_3DModelController/index.ts
@@ -4,6 +4,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/getAll",Services._3DModelService.get_3D_Models);
+router.get("/get/:uuid",Services._3DModelService.get_3D_Model);
 router.post("/add",Services._3DModelService.add_3D_Model);
 router.delete("/delete/:uuid",Services._3DModelService.delete_3D_Model);
 
--- src/Services/_3D_ModelService/index.ts
+++ src/Services/_3D_ModelService/index.ts
@@ -1,6 +1,17 @@
 import { Request,Response } from "express";
 import GenerateUUID from "../../Utilities/GenerateUUID";
 import _3D_ModelModel from "../../Models/_3D_Model";
+import { uploadFile } from "../../Configuration/MulterUpload";
+import { deleteFiles,retrieveFiles } from "../../Utilities/HandleFsElements";
+
+export const get_3D_Model = async (req:Request,res:Response) => {
+    const { uuid } = req.params;
+    const _3D_Model = await _3D_ModelModel.findOne({uuid: uuid});
+    if(!_3D_Model) throw new Error("_3D_Model not found");
+    const path = _3D_Model.url;
+    const binary = retrieveFiles(path);
+    res.send({file: binary, details : _3D_Model});
+}
 
 export const get_3D_Models = async (req:Request,res:Response) => {
     const _3D_Models = await _3D_ModelModel.find();
@@ -10,25 +21,40 @@ export const get_3D_Models = async (req:Request,res:Response) => {
 }
 
 const add_3D_Model = async (req:Request,res:Response) => {
-    const uuid = GenerateUUID();
-    const creationDate = new Date();
-    const { name, url } = req.body;
-    const _3D_Model = new _3D_ModelModel({
-        name: name,
-        url: url,
-        uuid: uuid,
-        creationDate: creationDate
-    });
-    await _3D_Model.save();
-    res.json({
-        message: "_3D_Model saved successfully",
-        _3D_Model
-    });
+    uploadFile.single("file")(req,res,async (err:any) => {
+        if(err) {
+            res.json({
+                message: "Error uploading _3D_Model"
+            });
+        }
+        else {
+            const uuid = GenerateUUID();
+            const creationDate = new Date();
+            const { name } = req.body;
+            const _3D_Model = new _3D_ModelModel({
+                name: name,
+                url: req.file?.filename,
+                uuid: uuid,
+                creationDate: creationDate
+            });
+            if(!req.file?.filename) throw new Error("Error uploading _3D_Model");
+            await _3D_Model.save();
+            res.json({
+                message: "_3D_Model saved successfully",
+                _3D_Model
+            });
+        } 
+    }
+    );
 }
 
 const delete_3D_Model = async (req:Request,res:Response) => {
     const { uuid } = req.params;
-    await _3D_ModelModel.findOneAndDelete({uuid});
+    if(!uuid) throw new Error("wrong uuid");
+    const file = await _3D_ModelModel.findOne({uuid: uuid});
+    if(!file) throw new Error("_3D_Model not found");
+    deleteFiles(file.url);
+    await file.deleteOne();
     res.json({
         message: "_3D_Model deleted successfully"
     });
@@ -37,5 +63,6 @@ const delete_3D_Model = async (req:Request,res:Response) => {
 export default {
     get_3D_Models,
     add_3D_Model,
-    delete_3D_Model
+    delete_3D_Model,
+    get_3D_Model
 }
